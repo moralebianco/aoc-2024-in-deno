39e117c7d3346a5466815ca94841e0d84cd0abfc
Nelson Morales <moralebianco@gmail.com>
2024-12-01 03:36:50 -0500

    Solve Day 1

--- src/01/mod.ts
+++ src/01/mod.ts
@@ -10,6 +10,15 @@ function foo(left: number[], right: number[]) {
   return sum;
 }
 
+function bar(left: number[], counts: Record<string, number>) {
+  let sum = 0;
+  for (const e of left) {
+    if (!counts[e]) continue;
+    sum += e * counts[e];
+  }
+  return sum;
+}
+
 if (import.meta.main) {
   // 1
   const [left, right] = (() => {
@@ -22,4 +31,15 @@ if (import.meta.main) {
     return [left, right];
   })();
   console.log("foo:", foo(left, right));
+
+  // 2
+  const [counts] = (() => {
+    const counts: Record<string, number> = {};
+    for (const e of right) {
+      if (!counts[e]) counts[e] = 0;
+      counts[e]++;
+    }
+    return [counts];
+  })();
+  console.log("bar:", bar(left, counts));
 }
