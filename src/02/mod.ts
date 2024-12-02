84f205a4460dfb97cb8eecd586216617eadd042f
Nelson Morales <moralebianco@gmail.com>
2024-12-02 08:51:37 -0500

    Solve Day 2

--- src/02/mod.ts
+++ src/02/mod.ts
@@ -15,6 +15,31 @@ function foo(reports: number[][]) {
   return sum;
 }
 
+function bar(reports: number[][]) {
+  let sum = 0;
+  function getIndex(report: number[]) {
+    const flag = report[0] < report[1];
+    for (let i = 0; i < report.length - 1; i++) {
+      let diff = report[i] - report[i + 1];
+      if (flag) diff *= -1;
+      if (1 > diff || diff > 3) return i;
+    }
+    return -1;
+  }
+  for (const r of reports) {
+    const i = getIndex(r);
+    if (
+      i == -1 ||
+      (i > 0 && getIndex(r.slice(0, i).concat(r.slice(i + 1))) == -1) ||
+      getIndex(r.slice(0, i + 1).concat(r.slice(i + 2))) == -1 ||
+      getIndex(r.slice(1)) == -1
+    ) {
+      sum += 1;
+    }
+  }
+  return sum;
+}
+
 if (import.meta.main) {
   const data = readByLine(import.meta.dirname + "/input");
   // 1
@@ -26,4 +51,6 @@ if (import.meta.main) {
     return matrix;
   })();
   console.log(foo(reports));
+  // 2
+  console.log(bar(reports));
 }
