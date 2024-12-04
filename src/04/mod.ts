92dab5948b9dbeb0c69d816440d04d2526d2a511
Nelson Morales <moralebianco@gmail.com>
2024-12-04 18:03:59 -0500

    Rename regex pattern variable

--- src/04/mod.ts
+++ src/04/mod.ts
@@ -3,37 +3,37 @@ import { readInput } from "../util.ts";
 function foo(puzzle: string[]) {
   let sum = 0;
   const size = puzzle.length;
-  const regPattern = /(?=(XMAS|SAMX))/g;
+  const regp = /(?=(XMAS|SAMX))/g;
 
   // Horizontal
   for (const h of puzzle) {
-    sum += [...h.matchAll(regPattern)].length;
+    sum += [...h.matchAll(regp)].length;
   }
 
   // Vertical
   for (let j = 0; j < size; j++) {
     const v = [...Array(size)].map((_, i) => puzzle[i].at(j)).join("");
-    sum += [...v.matchAll(regPattern)].length;
+    sum += [...v.matchAll(regp)].length;
   }
 
   // Diagonals
   for (let i = size - 4; i > 0; i--) {
     const dL = [...Array(size - i)].map((_, j) => puzzle[i + j].at(j)).join("");
-    sum += [...dL.matchAll(regPattern)].length;
+    sum += [...dL.matchAll(regp)].length;
   }
   for (let j = 0; j < size - 3; j++) {
     const dL = [...Array(size - j)].map((_, i) => puzzle[i].at(i + j)).join("");
-    sum += [...dL.matchAll(regPattern)].length;
+    sum += [...dL.matchAll(regp)].length;
   }
   for (let j = 3; j < size; j++) {
     const dR = [...Array(j + 1)]
       .map((_, i) => puzzle[i].at(j - i)).join("");
-    sum += [...dR.matchAll(regPattern)].length;
+    sum += [...dR.matchAll(regp)].length;
   }
   for (let i = 0; i < size - 4; i++) {
     const dR = [...Array(size - i)]
       .map((_, j) => puzzle[i + j].at(size - j)).join("");
-    sum += [...dR.matchAll(regPattern)].length;
+    sum += [...dR.matchAll(regp)].length;
   }
 
   return sum;
@@ -42,14 +42,14 @@ function foo(puzzle: string[]) {
 function bar(puzzle: string[]) {
   let sum = 0;
   const size = puzzle.length;
-  const regPattern = /(MAS|SAM)/g;
+  const regp = /(MAS|SAM)/g;
 
   for (let i = 1; i < size - 1; i++) {
     for (let j = 1; j < size - 1; j++) {
       const ch = puzzle[i].charAt(j);
       const dL = puzzle[i - 1].at(j - 1) + ch + puzzle[i + 1].at(j + 1);
       const dR = puzzle[i + 1].at(j - 1) + ch + puzzle[i - 1].at(j + 1);
-      if (dL.match(regPattern) && dR.match(regPattern)) sum++;
+      if (dL.match(regp) && dR.match(regp)) sum++;
     }
   }
 
