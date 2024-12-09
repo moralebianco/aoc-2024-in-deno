12e33e7c14bf7692ed2efeb2fed413e245255871
Nelson Morales <moralebianco@gmail.com>
2024-12-09 00:14:22 -0500

    Refactor

--- src/08/mod.ts
+++ src/08/mod.ts
@@ -2,7 +2,40 @@ import { readLines } from "../util.ts";
 
 type Pos = [x: number, y: number];
 
-function foo(map: string[][]) {
+function getPositions(map: string[][], arr: Pos[], resonant: boolean) {
+  const pos: Pos[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let _0 = [arr[i][0], arr[i][1]];
+      let _1 = [arr[j][0], arr[j][1]];
+      const xDiff = _1[0] - _0[0];
+      const yDiff = _1[1] - _0[1];
+      let $0: Pos = [_0[0] - xDiff, _0[1] - yDiff];
+      while (
+        $0[0] >= 0 && $0[0] < map.length &&
+        $0[1] >= 0 && $0[1] < map[0].length
+      ) {
+        pos.push($0);
+        if (!resonant) break;
+        _0 = $0;
+        $0 = [_0[0] - xDiff, _0[1] - yDiff];
+      }
+      let $1: Pos = [_1[0] + xDiff, _1[1] + yDiff];
+      while (
+        $1[0] >= 0 && $1[0] < map.length &&
+        $1[1] >= 0 && $1[1] < map[0].length
+      ) {
+        pos.push($1);
+        if (!resonant) break;
+        _1 = $1;
+        $1 = [_1[0] + xDiff, _1[1] + yDiff];
+      }
+    }
+  }
+  return pos;
+}
+
+function foo(map: string[][], resonant: boolean) {
   let sum = 0;
   const dict = new Map<string, Pos[]>();
   for (let i = 0; i < map.length; i++) {
@@ -13,83 +46,13 @@ function foo(map: string[][]) {
     }
   }
   for (const [_, v] of dict.entries()) {
-    const pos = getPositions(v);
+    const pos = getPositions(map, v, resonant);
     for (const [x, y] of pos) map[x][y] = "#";
   }
   for (const row of map) {
     for (const e of row) if (e === "#") sum++;
   }
   return sum;
-  function getPositions(arr: Pos[]) {
-    const pos: Pos[] = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        const xDiff = arr[j][0] - arr[i][0];
-        const yDiff = arr[j][1] - arr[i][1];
-        const $0: Pos = [arr[i][0] - xDiff, arr[i][1] - yDiff];
-        const $1: Pos = [arr[j][0] + xDiff, arr[j][1] + yDiff];
-        if (
-          $0[0] >= 0 && $0[0] < map.length &&
-          $0[1] >= 0 && $0[1] < map[0].length
-        ) pos.push($0);
-        if (
-          $1[0] >= 0 && $1[0] < map.length &&
-          $1[1] >= 0 && $1[1] < map[0].length
-        ) pos.push($1);
-      }
-    }
-    return pos;
-  }
-}
-
-function bar(map: string[][]) {
-  let sum = 0;
-  const dict = new Map<string, Pos[]>();
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[0].length; j++) {
-      if (map[i][j] === ".") continue;
-      if (!dict.has(map[i][j])) dict.set(map[i][j], []);
-      dict.get(map[i][j])?.push([i, j]);
-    }
-  }
-  for (const [_, v] of dict.entries()) {
-    const pos = getPositions(v);
-    for (const [x, y] of pos) map[x][y] = "#";
-  }
-  for (const row of map) {
-    for (const e of row) if (e !== ".") sum++;
-  }
-  return sum;
-  function getPositions(arr: Pos[]) {
-    const pos: Pos[] = [];
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = i + 1; j < arr.length; j++) {
-        let _0 = [arr[i][0], arr[i][1]];
-        let _1 = [arr[j][0], arr[j][1]];
-        const xDiff = _1[0] - _0[0];
-        const yDiff = _1[1] - _0[1];
-        let $0: Pos = [_0[0] - xDiff, _0[1] - yDiff];
-        while (
-          $0[0] >= 0 && $0[0] < map.length &&
-          $0[1] >= 0 && $0[1] < map[0].length
-        ) {
-          pos.push($0);
-          _0 = $0;
-          $0 = [_0[0] - xDiff, _0[1] - yDiff];
-        }
-        let $1: Pos = [_1[0] + xDiff, _1[1] + yDiff];
-        while (
-          $1[0] >= 0 && $1[0] < map.length &&
-          $1[1] >= 0 && $1[1] < map[0].length
-        ) {
-          pos.push($1);
-          _1 = $1;
-          $1 = [_1[0] + xDiff, _1[1] + yDiff];
-        }
-      }
-    }
-    return pos;
-  }
 }
 
 if (import.meta.main) {
@@ -97,8 +60,8 @@ if (import.meta.main) {
   let map: string[][];
   // 1
   map = data.map((r) => r.split(""));
-  console.log(foo(map));
+  console.log(foo(map, false));
   // 2
   map = data.map((r) => r.split(""));
-  console.log(bar(map));
+  console.log(foo(map, true));
 }
