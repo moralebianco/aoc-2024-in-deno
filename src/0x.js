e0c8e393a7a31787f7bd3aa9cdc1d3ce75363f83
Nelson Morales <moralebianco@gmail.com>
2025-01-02 21:58:46 -0500

    Add function to read matrix

--- src/0x.js
+++ src/0x.js
@@ -1,3 +1,5 @@
 export const RF = (_) => Deno.readTextFileSync("./input");
 
+export const RM = (_) => RF().split("\n").map((l) => l.split(""));
+
 export const SM = (M) => M.forEach((r) => console.debug(r.join("")));
